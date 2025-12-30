06d71d5677ac149d4485f98092342cc34c26f60f
handy <lihandi@gmail.com>
2025-12-30 23:07:45 +0800

    chore: 引入自定义 Button 组件到登录页面

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import Button from '@/components/ui/Button'
 
 export default function LoginPage() {
   const router = useRouter()
